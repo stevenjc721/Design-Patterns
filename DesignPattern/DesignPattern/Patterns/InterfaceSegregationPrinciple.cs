9a0de1ea036b58d0ac19a956e8754a6ffeb8cc91
steven.carver <stevenjc721@gmail.com>
2018-08-01 16:20:02 -0400

    Section 1

---
+++ DesignPattern/DesignPattern/Patterns/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Interface Segregation Principle
+ * your Interface should be segregated so that you don't have to Implement methods you don't need.
+ */
+namespace DesignPattern.Patterns
+{
+    public class Document
+    {
+
+    }
+    //Ok for multiFunctionPrinter but not for older devices
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+    }
+    /*
+     * OldFashionPrinter can't scan or fax but you have to implement because all call are in the same Interface.
+     */
+    public class OldFashionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    /*
+     * One solution is to have multiple interface and inherit from them all
+     */
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    /*
+     * A Second Solution is to have a "Master" interface that inherits from other interfaces
+     */
+    public interface IMultiFunctionDevice : IScanner, IPrinter //....
+    {
+    }
+
+    public class MultifunctionMachineDevice : IMultiFunctionDevice
+    {
+        /*
+         * 1 Option of Implementation
+         
+        public void Print(Document d)
+        {
+            //
+        }
+        public void Scan(Document d)
+        {
+            //
+        }
+        */
+
+        /*
+         * Option 2 Delegating the calls Decorator Pattern
+         */
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultifunctionMachineDevice(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }// decorator
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }// decorator
+    }
+    class InterfaceSegregationPrinciple
+    {
+    }
+}
---
+++ DesignPattern/DesignPattern/Patterns/LiskovSubstitutionPrinciple.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Liskov Substitution Principle: You should be able to substitute a base type for a subtype
+ * In this case Rectangle for Square
+ */
+namespace DesignPattern.Patterns
+{
+    public class Rectangle {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+    public class Square : Rectangle
+    {
+        //Switch new to override 
+        public override int Width {
+            set { base.Width = base.Height = value; }
+        }
+        public override int Height
+        {
+            set { base.Height = base.Width = value; }
+        }
+    }
+    class LiskovSubstitutionPrinciple
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle();
+            Console.Out.WriteLine($"{rc} has area {Area(rc)}");
+            // Rectangle and Square should be interchangible but if you change the Area will not be correct
+            Square sq = new Square();
+            sq.Width = 4;
+            Console.Out.WriteLine($"{sq} has area {Area(sq)}");
+            
+            //How to fix? use Override in Square and make Height/Width Virtual in Rectangle
+        }
+    }
+}
